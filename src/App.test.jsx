75afdc196446cd2aedd1bbeee920dd186380a5c1
Alice Hsing <alicehsing@gmail.com>
2022-05-13 13:47:36 -0700

    added edit button test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -11,12 +11,16 @@ beforeEach(() => {
   render(<App />);
 });
 
-it('component test - renders a header consisted of a heading element and a button', () => {
+it('component test - renders a header consisted of a heading element and a button as well as list of pre-set shopping items ', () => {
   screen.getByRole('heading', {
     name: 'My Shopping List',
   });
 
   screen.getByLabelText('clear cart');
+
+  screen.getByText(/Corn/i);
+  screen.getByText('Steak 🥩');
+  screen.getByText('Ice Cream 🍦');
 });
 
 it('should be able to add a new item to the shopping list', () => {
@@ -32,5 +36,21 @@ it('should be able to add a new item to the shopping list', () => {
   //new item gets rendered on the list view
   expect(screen.getByText('Meat 🍗')).toBeInTheDocument();
 });
+
+it('should be able to edit an existing item', async () => {
+  const editButton = screen.getByLabelText('Edit Corn 🌽');
+  userEvent.click(editButton);
+  const editInput = screen.getByLabelText('Edit field');
+  userEvent.type(editInput, '4 Corns 🌽');
+  const saveButton = screen.getByLabelText('Save changes');
+  userEvent.click(saveButton);
+  const editItem = await screen.findByText(/4 Corns/i);
+  expect(editItem).toBeInTheDocument();
+  screen.debug();
+});
+
+// it('should be able to mark a shopping item as done with strike through on text')
+
+// it('should be able to delete an item on the list')
 
 // it('should count added items and reset cart to 0');
