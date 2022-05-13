a0f2ae83a44e801e76204b468ce4a90f4e823424
Alice Hsing <alicehsing@gmail.com>
2022-05-12 21:55:16 -0700

    fixed form Add Item button, removed onClick() in button

--- src/views/List.jsx
+++ src/views/List.jsx
@@ -2,15 +2,7 @@ import { useState } from 'react';
 import Item from '../components/Items/Item';
 import { useItems } from '../context/ListProvider';
 
-// const initialItems = [
-//   { id: 0, text: 'Corn 🌽', done: false },
-//   { id: 1, text: 'Steak 🥩', done: false },
-//   { id: 2, text: 'Ice Cream 🍦', done: false },
-// ];
-
 export default function List() {
-  // bring in useReducer hook, pass in reducer function and set initialItems state
-  // const [items, dispatch] = useReducer(reducer, initialItems);
   const [newItem, setNewItem] = useState('');
   const { items, handleAddItem, handleDeleteItem, handleUpdateItem } =
     useItems();
@@ -21,12 +13,6 @@ export default function List() {
     handleAddItem(newItem);
     setNewItem('');
   };
-
-  // // Delete an item from list
-  // const handleDeleteItem = (id) => {
-  //   console.log(`Delete item ${id}`);
-  //   dispatch({ type: 'DELETE_ITEM', payload: { id } });
-  // };
 
   return (
     <>
@@ -39,7 +25,7 @@ export default function List() {
           value={newItem}
           onChange={(event) => setNewItem(event.target.value)}
         />
-        <button onClick={handleAddItem}>Add Item</button>
+        <button>Add Item</button>
       </form>
       <section>
         <p>Display list below</p>
